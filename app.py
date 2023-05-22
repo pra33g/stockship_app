9a9e46c5500f67e6d23d7cc2d28204e5b0da6398
Prakhar Gupta <pguptaking@gmail.com>
2023-05-22 22:29:55 +0530

    calculate ticker names

--- app.py
+++ app.py
@@ -1,29 +1,53 @@
 from smartapi import SmartConnect
 import helper
-from datetime import date
+import datetime
+api = SmartConnect(api_key='zYJrbBfd')
 
-print(date.today())
-api.generateSession('P892158', '5542', '838285')
-# api = SmartConnect(
-# api.generateSession('P892158', '5542', '696317')
-# )
-# api.generateSession('P892158', '5542', '165412')
+api.generateSession('P892158', '5542', '015448')
 
-# data = api.ltpData("NSE", "NIFTY", "26000")
-# ltp = data['data']['ltp']
-# print(ltp)
-# const = 250
-# base = 50
-# ce1 = helper.roundUp(ltp + 250, base)
-# pe1 = helper.roundDown(ltp - 250, base)
+data = api.ltpData("NSE", "NIFTY", "26000")
+ltp = data['data']['ltp']
+print("ltp=", ltp)
+const = 250
+base = 50
+ce1 = helper.roundUp(ltp + 250, base)
+pe1 = helper.roundDown(ltp - 250, base)
 
-# #sell
-# print(f"sell {ce1} ")
+ce2 = helper.roundUp(ltp + (2 * 250), base)
+pe2 = helper.roundDown(ltp - (2 * 250), base)
 
 
-# #buy
+with open("holidays") as holidays:
+    lines = holidays.readlines()
 
-# ce2 = helper.roundUp(ltp + (2 * 250), base)
-# pe2 = helper.roundDown(ltp - (2 * 250), base)
+for i,line in enumerate(lines):
+    lines[i] = line.strip()
 
+gotExitDate = False
+exitDate = None
+exitDate_unformatted = None
+d = 7
+today = datetime.date.today()
+while not gotExitDate:
+    week = datetime.timedelta(days=d)
+    exitDate_unformatted = today + week
+    exitDate = f"{'{0:02d}'.format(exitDate_unformatted.day)}-{helper.Month[exitDate_unformatted.month - 1]}-{exitDate_unformatted.year}"
+    if exitDate not in lines:
+        gotExitDate = True
+    else:
+        d -= 1
 
+edu = exitDate_unformatted
+#sell ce1 pe1
+#buy ce2 pe2
+
+#sell
+#eg NIFTY11AUG2216850PE.NFO
+ce1_ticker = f"NIFTY{edu.day}{helper.Month[edu.month - 1]}{str(edu.year)[-2:]}{ce1}.NFO"
+pe1_ticker = f"NIFTY{edu.day}{helper.Month[edu.month - 1]}{str(edu.year)[-2:]}{pe1}.NFO"
+#buy
+ce2_ticker = f"NIFTY{edu.day}{helper.Month[edu.month - 1]}{str(edu.year)[-2:]}{ce2}.NFO"
+pe2_ticker = f"NIFTY{edu.day}{helper.Month[edu.month - 1]}{str(edu.year)[-2:]}{pe2}.NFO"
+
+print("sell:", ce1_ticker, pe1_ticker)
+print("buy:", ce2_ticker, pe2_ticker)
--- test.py
+++ test.py
@@ -2,15 +2,23 @@ import datetime
 import helper
 # print(helper.Month[date.today().month - 1])
 
-week = datetime.timedelta(days=7)
-today = datetime.date.today()
-exitDate_unformatted = today + week
-
-exitDate = f"{exitDate.day}-{helper.Month[exitDate.month - 1]}-{exitDate.year}"
 
 with open("holidays") as holidays:
     lines = holidays.readlines()
 
 for i,line in enumerate(lines):
     lines[i] = line.strip()
-    
+
+gotExitDate = False
+exitDate = None
+d = 7
+today = datetime.date.today()
+while not gotExitDate:
+    week = datetime.timedelta(days=d)
+    exitDate_unformatted = today + week
+    exitDate = f"{'{0:02d}'.format(exitDate_unformatted.day)}-{helper.Month[exitDate_unformatted.month - 1]}-{exitDate_unformatted.year}"
+    print(exitDate)
+    if exitDate not in lines:
+        gotExitDate = True
+    else:
+        d -= 1
